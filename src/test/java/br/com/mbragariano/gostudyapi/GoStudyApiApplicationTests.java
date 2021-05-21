e283a49b654812f8624868b47ef2294e75e6db4b
Matheus Braga <mbraga@grupodimed.com.br>
2021-05-21 17:11:20 -0300

    Feat: Primeiro commit

---
+++ src/test/java/br/com/mbragariano/gostudyapi/GoStudyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mbragariano.gostudyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoStudyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
